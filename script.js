8eb0c60b6f5b9a11839c0fb9abf36be1d059a1e5
andzel0 <115092884+andzel0@users.noreply.github.com>
2022-11-23 19:37:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// THE HELLO WORLD EXERCISE
+
+// var code = "translate"; 
+// if (code == "de") {
+// console.log("Hallo Welt");
+// };
+
+
+// SEASONS
+// var seasons = prompt("ENTER A MONTH");
+
+// if (seasons == "september" || seasons == "october" || seasons == "november") {
+//     console.log("The season is Autumn");
+
+// }else if (seasons == "december" || seasons == "january" || seasons == "february") {
+//     console.log("The season is Winter");
+
+// }else if (seasons == "march" || seasons == "april" || seasons == "may") {
+//     console.log("The season is Spring");
+
+// }else if (seasons == "june" || seasons == "july" || seasons == "august") {
+//     console.log("The season is Summer");
+
+// }else {
+//     console.error("unknown");
+// }
+
+
+
+// MUSICIANS
+var band_members_num = 7;
+switch (band_members_num) {
+case 1:
+console.log(" This band is one ");
+
+break;
+case 2:
+console.log("This band is two");
+    
+break;
+case 3:
+console.log("This band is three");
+    
+break;
+case 4:
+console.log("This band is four");
+    
+break;
+case 5:
+console.log("This band is five");
+    
+break;
+case 6:
+console.log("This band is six");
+    
+break;
+case 7:
+console.log("This band is seven");
+   
+break;
+case 8:
+console.log("This band is eight");
+    
+break;
+case 9:
+console.log("This band is nine");
+    
+break;
+case 10:
+console.log("This band is ten");
+        
+break;
+default: console.log("Error 404");
+    };
